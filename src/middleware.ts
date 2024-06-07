50f084c2bb2176224688ed615b58b0bbfd85f8ba
leduygit <leduyanh04@gmail.com>
2024-06-07 17:42:07 +0700

    fix: detele middleware

--- src/middleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { updateSession } from '@/lib/backend/authentication/session';
-
-export async function middleware(request: NextRequest): Promise<NextResponse> {
-  return await updateSession(request);
-}
