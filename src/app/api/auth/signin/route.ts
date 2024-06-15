ce8bd97ba1b02b42042a566f28c836e1cab27abc
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2024-06-15 12:53:57 +0700

    add: swagger

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -2,6 +2,61 @@ import { NextRequest, NextResponse } from "next/server";
 import { loginUser } from "@/lib/backend/authentication/authentication";
 import { jsonHeader } from "@/lib/backend/header/jsonheader";
 
+/**
+ * @swagger
+ * /api/hello:
+ *   get:
+ *     description: Returns the hello world
+ *     responses:
+ *       200:
+ *         description: Hello World!
+ */
+
+/**
+*   @swagger
+*   /api/auth/signin:
+*     post:
+*       description: Sign in a user and return a token
+*       requestBody:
+*           required: true
+*           content:
+*             application/json:
+*               schema:
+*                   type: object
+*                   properties:
+*                     email:
+*                       type: string
+*                     password:
+*                       type: string
+*       responses:
+*         200:
+*           description: Successfully signed in and returned a token
+*           content:
+*             application/json:
+*               schema:
+*                 type: object
+*                 properties:
+*                   error:
+*                     type: string
+*                     description: Empty string
+*                   token:
+*                     type: string
+*                     description: JWT token
+*         401:
+*           description: Invalid email or password
+*           content:
+*             application/json:
+*               schema:
+*                 type: object
+*                 properties:
+*                   error:
+*                     type: string
+*                     description: Invalid email or password
+*                   token:
+*                     type: string
+*                     description: Empty string
+*/
+
 export async function POST(request: NextRequest) {
   try {
     const data = await request.json();
