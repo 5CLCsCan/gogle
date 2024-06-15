5484d4fdcd01ea3cd67e5b138488035eb76115ce
CaoHuuKhuongDuy <khuongduy2121@gmail.com>
2024-06-15 13:06:41 +0700

    fix: format swagger yaml

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -5,56 +5,56 @@ import { jsonHeader } from "@/lib/backend/header/jsonheader";
 /**
  * @swagger
  * /api/hello:
- *   get:
- *     description: Returns the hello world
- *     responses:
- *       200:
- *         description: Hello World!
+ *  get:
+ *      description: Returns the hello world
+ *      responses:
+ *          200:
+ *              description: Hello World     
  */
 
 /**
 *   @swagger
 *   /api/auth/signin:
-*     post:
+*    post:
 *       description: Sign in a user and return a token
 *       requestBody:
 *           required: true
 *           content:
-*             application/json:
-*               schema:
-*                   type: object
-*                   properties:
-*                     email:
-*                       type: string
-*                     password:
-*                       type: string
+*               application/json:
+*                   schema:
+*                       type: object
+*                       properties:
+*                           email:
+*                               type: string
+*                           password:
+*                               type: string
 *       responses:
-*         200:
-*           description: Successfully signed in and returned a token
-*           content:
-*             application/json:
-*               schema:
-*                 type: object
-*                 properties:
-*                   error:
-*                     type: string
-*                     description: Empty string
-*                   token:
-*                     type: string
-*                     description: JWT token
-*         401:
-*           description: Invalid email or password
-*           content:
-*             application/json:
-*               schema:
-*                 type: object
-*                 properties:
-*                   error:
-*                     type: string
-*                     description: Invalid email or password
-*                   token:
-*                     type: string
-*                     description: Empty string
+*           200:
+*               description: Successfully signed in and returned a token
+*               content:
+*                   application/json:
+*                       schema:
+*                           type: object
+*                           properties:
+*                               error:
+*                                   type: string
+*                                   description: Empty string
+*                               token:
+*                                   type: string
+*                                   description: JWT token
+*           401:
+*               description: Invalid email or password
+*               content:
+*                   application/json:
+*                       schema:
+*                           type: object
+*                           properties:
+*                               error:
+*                                   type: string
+*                                   description: Invalid email or password
+*                               token:
+*                                   type: string
+*                                   description: Empty string
 */
 
 export async function POST(request: NextRequest) {
