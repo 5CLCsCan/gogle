284e81563f348667ccd7913bbd08f650e0d5d592
leduygit <leduyanh04@gmail.com>
2024-06-17 12:24:09 +0700

    doc: add auth api documentation

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,20 +1,89 @@
-import { NextRequest, NextResponse } from "next/server";
-import { registerUser } from "@/lib/backend/authentication/authentication";
-import { jsonHeader } from "@/lib/backend/header/jsonheader";
+import { NextRequest, NextResponse } from 'next/server'
+import { registerUser } from '@/lib/backend/authentication/authentication'
+import { jsonHeader } from '@/lib/backend/header/jsonheader'
+
+/**
+ *   @swagger
+ *   /api/auth/signup:
+ *    post:
+ *       description: Sign in a user and return a token
+ *       requestBody:
+ *           required: true
+ *           content:
+ *               application/json:
+ *                   schema:
+ *                       type: object
+ *                       properties:
+ *                           username:
+ *                              type: string
+ *                              example: user
+ *                           email:
+ *                               type: string
+ *                               example: user@gmail.com
+ *                           password:
+ *                               type: string
+ *                               example: IAMPASSWORD
+ *       responses:
+ *           200:
+ *               description: Successfully signed up and returned a token
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           type: object
+ *                           properties:
+ *                               error:
+ *                                   type: string
+ *                                   description: Empty string
+ *                                   example: ""
+ *                               token:
+ *                                   type: string
+ *                                   description: JWT token
+ *                                   example: IAMTOKEN12134
+ *           409:
+ *               description: Email already exists
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           type: object
+ *                           properties:
+ *                               error:
+ *                                   type: string
+ *                                   description: Email already exists
+ *                                   example: Email already exists
+ *                               token:
+ *                                   type: string
+ *                                   description: Empty string
+ *                                   example: ""
+ *           500:
+ *               description: Internal Server Error
+ *               content:
+ *                   application/json:
+ *                       schema:
+ *                           type: object
+ *                           properties:
+ *                               error:
+ *                                   type: string
+ *                                   description: Internal Server Error
+ *                                   example: Internal Server Error
+ *                               token:
+ *                                   type: string
+ *                                   description: Empty string
+ *                                   example: ""
+ */
 
 export async function POST(request: NextRequest) {
   try {
-    const data = await request.json();
-    const result = await registerUser(data);
-    if (result.error == "Email already exists") {
-      return new NextResponse(JSON.stringify(result), { status: 409 });
+    const data = await request.json()
+    const result = await registerUser(data)
+    if (result.error == 'Email already exists') {
+      return new NextResponse(JSON.stringify(result), { status: 409 })
     }
-    return new NextResponse(JSON.stringify(result), jsonHeader);
+    return new NextResponse(JSON.stringify(result), jsonHeader)
   } catch (err) {
-    console.error("Error in signup route:", err);
+    console.error('Error in signup route:', err)
     return new NextResponse(
-      JSON.stringify({ error: "Internal Server Error", token: "" }),
-      jsonHeader
-    );
+      JSON.stringify({ error: 'Internal Server Error', token: '' }),
+      jsonHeader,
+    )
   }
 }
--- src/lib/backend/updateData/createTrip.ts
+++ src/lib/backend/updateData/createTrip.ts
@@ -1,27 +1,28 @@
-import TripModel from '@/models/TripSchema';
-import { connectDB, addData } from '@/lib/database';
+import TripModel from '@/models/TripSchema'
+import { connectDB, addData } from '@/lib/backend/database'
 
 export interface CreateTripData {
-    userEmail: string;
-    startDate: Date;
-    startTime: string;
-    locations?: string[];
-    sharedEmails: string[];
+  userEmail: string
+  startDate: Date
+  startTime: string
+  locations?: string[]
+  sharedEmails: string[]
 }
 
+export default async function createTrip(
+  data: CreateTripData,
+): Promise<boolean> {
+  await connectDB()
+  const { userEmail, startDate, startTime, locations, sharedEmails } = data
+  const newTrip = new TripModel({
+    userEmail: userEmail,
+    startDate: startDate,
+    startTime: startTime,
+    locations: locations,
+    sharedEmails: sharedEmails,
+  })
 
-export default async function createTrip(data: CreateTripData): Promise<boolean> {
-    await connectDB();
-    const { userEmail, startDate, startTime, locations, sharedEmails } = data;
-    const newTrip = new TripModel({
-        userEmail: userEmail,
-        startDate: startDate,
-        startTime: startTime,
-        locations: locations,
-        sharedEmails: sharedEmails
-    });
-
-    console.log("new trip:" + newTrip);
-    const status = await addData(newTrip);
-    return status;
+  console.log('new trip:' + newTrip)
+  const status = await addData(newTrip)
+  return status
 }
