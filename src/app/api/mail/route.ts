4bc3129020ce032d5b646ee84014f2ab858a8378
leduygit <leduyanh04@gmail.com>
2024-08-16 15:49:31 +0700

    doc: add swagger for verify email api

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -10,6 +10,60 @@ import { NextRequest, NextResponse } from 'next/server'
 
 import { jsonHeader } from '@/lib/backend/header/jsonheader'
 
+/**
+ * @swagger
+ * /api/mail:
+ *   post:
+ *     description: Send verification email
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: hcmusgogle@gmail.com
+ *               username:
+ *                 type: string
+ *                 example: user
+ *     responses:
+ *       200:
+ *         description: Email sent successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Email sent successfully
+ *                   example: Email sent successfully
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: User not found
+ *                   example: User not found
+ *       500:
+ *         description: Internal Server Error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Internal Server Error
+ *                   example: Internal Server Error
+ */
+
 export async function POST(request: NextRequest) {
   const { email, username } = await request.json()
 
--- src/app/api/verify/route.ts
+++ src/app/api/verify/route.ts
@@ -4,6 +4,78 @@ import UserModel from '@/models/UserSchema'
 
 import { NextApiRequest, NextApiResponse } from 'next'
 
+// swagger
+
+/**
+ * @swagger
+ * /api/verify:
+ *   get:
+ *     description: Verify user
+ *     parameters:
+ *       - in: query
+ *         name: token
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Verify token
+ *     responses:
+ *       200:
+ *         description: User verified
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: User verified
+ *                   example: User verified
+ *       404:
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: User not found
+ *                   example: User not found
+ *       400:
+ *         description: Token not valid
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Token not valid
+ *                   example: Token not valid
+ *       406:
+ *         description: Token expired
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Token expired
+ *                   example: Token expired
+ *       451:
+ *         description: Token not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Token not found
+ *                   example: Token not found
+ */
+
 export async function GET(req: Request) {
   const url = new URL(req.url)
   const token = url.searchParams.get('token')
@@ -11,16 +83,19 @@ export async function GET(req: Request) {
   console.log(token)
 
   if (!token) {
-    return new Response(JSON.stringify({ error: 'Missing token parameter' }), {
-      status: 400,
-    })
+    return new Response(
+      JSON.stringify({ message: 'Missing token parameter' }),
+      {
+        status: 451,
+      },
+    )
   }
 
   // find user by verify token
   const user = await findData(UserModel, { verifyToken: token })
 
   if (!user) {
-    return new Response(JSON.stringify({ error: 'User not found' }), {
+    return new Response(JSON.stringify({ message: 'User not found' }), {
       status: 404,
     })
   }
@@ -28,14 +103,14 @@ export async function GET(req: Request) {
   // if token is expired
   const now = new Date()
   if (!user[0].verifyTokenExpires) {
-    return new Response(JSON.stringify({ error: 'Token not valid' }), {
+    return new Response(JSON.stringify({ message: 'Token not valid' }), {
       status: 400,
     })
   }
 
   if (user[0].verifyTokenExpires < now) {
-    return new Response(JSON.stringify({ error: 'Token expired' }), {
-      status: 400,
+    return new Response(JSON.stringify({ message: 'Token expired' }), {
+      status: 406,
     })
   }
 
@@ -46,7 +121,7 @@ export async function GET(req: Request) {
   )
 
   if (!updateUser) {
-    return new Response(JSON.stringify({ error: 'User not found' }), {
+    return new Response(JSON.stringify({ message: 'User not found' }), {
       status: 404,
     })
   }
